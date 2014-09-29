bc87b2578e0e6dd6241ac4a7b0d7f03c571fcd9c
Annie <anniejw6@gmail.com>
2014-09-28 21:18:00 -0400

    vary strength

--- Simulation/sim_run.R
+++ Simulation/sim_run.R
@@ -4,13 +4,15 @@ source('sim_func.R')
 
 set.seed(628)
 
+str <- c(81:71, 70, 70, 69:59)
+
 #rand  <- sim(5000, 'random', sdev = 7)
-power <- sim(5000, 'power')
-fold  <- sim(5000, 'fold')
-envel <- sim(5000, 'envelope')
-pseud <- sim(5000, 'pseudo-rand')
+power <- sim(5000, 'power', str = sample(str))
+fold  <- sim(5000, 'fold', str = sample(str))
+envel <- sim(5000, 'envelope', str = sample(str))
+pseud <- sim(5000, 'pseudo-rand', str = sample(str))
 
 rm(list=lsf.str())
 
 #save.image('all_r1.RData')
-save.image("~/Dropbox/all_R1.RData")
+save.image("all_R1.RData")
