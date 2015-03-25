05ffb42fbe9b159b473ee988d5d70ab121136bd9
Annie <anniejw6@gmail.com>
2015-03-25 08:27:03 -0400

    wrappers on wrappers

--- Simulation/sim_func.R
+++ Simulation/sim_func.R
@@ -1,258 +1,6 @@
 library(plyr)
 library(reshape)
 
-##### Wrapper #########
-sim <- function(num.trials,
-                type = c('random', 'power', 'fold', 'envelope', 'pseudo-rand'),
-                str = c(81:71, 70, 70, 69:59),
-                qualwin = 14,
-                sdev = 10,
-                teams = NULL,
-                pp = F){
-  
-  type <- match.arg(type)
-  
-  # Set base values
-  num.teams = 24
-  base = 1 # Minimum number of ballots that teams start out with for wpb
-  
-  # Set Team Values and Impermissibles
-  if(is.null(teams)){
-    teams <- data.frame(
-      University = 1:num.teams,
-      Team = 1:num.teams,
-      TeamName = 1:num.teams
-    )
-  }
-  
-  amta.tot <- NULL
-  wpb.tot <- NULL
-  meta.tot <- NULL
-  
-  if(type == 'random'){
-    num.trials <- num.trials/length(sdev)
-  } 
-  
-  for (trial in 1:num.trials){
-    print(paste("TRIAL", trial))
-    for (k in 1:length(sdev)){
-      amta <- NULL
-      wpb <- NULL
-      
-      #Generate data frames
-      amta <- data.frame(team = 1:num.teams)
-      amta[,genNames("AMTA")] <- NA
-      
-      wpb <- data.frame(team = 1:num.teams, base = base)
-      wpb[,genNames("wpb")] <- NA
-      
-      # Generate Teams and Strength
-      if(type == 'random'){
-        
-        amta$str <- wpb$str <- round(rnorm(num.teams, 70, sdev[k]), 0)
-        amta$true_rank <- wpb$true_rank <- rank(-amta$str)
-        
-      } else {
-        
-        amta$str <- wpb$str <- sample(str)
-        amta$true_rank <- wpb$true_rank <- rank(-amta$str)
-      }
-      
-      # Pair Round 1 
-      # Round 1
-      r1 <- c("r1side", "r1opp")
-      
-      if(type %in% c('random', 'pseudo-rand')){
-        amta[, r1] <- wpb[, r1] <- genR1(amta, num.teams)
-      } else if (type == 'power'){
-        amta[, r1] <- wpb[, r1] <- r1power(amta)
-      } else if (type == 'fold'){
-        amta[, r1] <- wpb[, r1] <- r1fold(amta)
-      } else if (type == 'envelope'){
-        amta[, r1] <- wpb[, r1] <- r1envelope(amta)
-      }
-      
-      # Fix Sides
-      amta$r1side <- ifelse(amta$r1side == 1, 'P', 'D')
-      wpb$r1side <- ifelse(wpb$r1side == 1, 'P', 'D')
-      
-      for(i in 1:4){
-        #print(paste("ROUND", i))
-        
-        ##### Simulate Outcomes #####
-        # Calculate PD
-        amta[, paste0('r',i, c("opp.str", "pd"))] <- calcPD(amta, i)
-        wpb[, paste0('r',i, c("opp.str", "pd"))] <- calcPD(wpb, i)
-        
-        # Calculate WPB Measures
-        wpb[, paste("r", i, c("round.pb", "cum.pb"), sep ="")] <- calcPB(i, wpb, qualwin)
-        wpb[, c(paste0("r", 1:4, "round.wpb"), paste0("r", i, "cum.wpb"))] <- calcWPB(i, wpb)
-        
-        # Calculate AMTA Measures 
-        amta[, paste0("r", i, c("round.bal", "cum.bal"))] <- calcBal(i, amta)
-        amta[, c(paste0("r", 1:4, "round.cs"), paste0("r", i, "cum.cs"))] <- calcCS(i, amta)
-        
-        # Calculate Point Differential
-        amta[, paste0("r", i, "cum.pd")] <- calcCumPD(amta)
-        wpb[, paste0("r", i, "cum.pd")] <- calcCumPD(wpb)
-        
-        # Rank Teams (not for Pairing)
-        amta[,paste0("r", i,"rank")] <- with(amta, rankTrad(get(paste0("r", i, "cum.bal")), 
-                                                            get(paste0("r", i, "cum.cs")),
-                                                            get(paste0("r", i, "cum.pd"))))
-        wpb[,paste0("r", i,"rank")] <- with(wpb, rankWPB(get(paste0("r", i, "cum.wpb")), 
-                                                         get(paste0("r", i, "cum.pb")),
-                                                         get(paste0("r", i, "cum.pd"))))
-        
-        #Pair next round
-        if(i < 4){
-          
-          ##### Pairing #######
-          coinTie <- 'heads'
-          coinR3  <- 'heads'
-          
-          meta <- data.frame(
-            trial = trial,
-            coinTie = 'heads',
-            coinR3 = 'heads'
-          )
-          
-          nextRound <- i + 1
-          
-          # Re-Format
-          amtatab <- tabSumm(amta, amta = T, round = nextRound)
-          wpbtab  <- tabSumm(wpb, amta = F, round = nextRound)
-          
-          # Rank Teams
-          amtatab$rank <- rankMT(dat = amtatab, 
-                                 crit1 = 'crit1', crit2 = 'crit2', crit3 = 'crit3', 
-                                 r = nextRound, coinflip = coinTie)
-          
-          wpbtab$rank <- rankMT(dat = amtatab, 
-                                crit1 = 'crit1', crit2 = 'crit2', crit3 = 'crit3', 
-                                r = nextRound, coinflip = coinTie)
-          
-          # Define Impermissibles
-          amtaImpermiss <- rbind(sameSchool(teams),
-                                 pastOpp(amta, nextRound))
-          
-          wpbImpermiss <- rbind(sameSchool(teams),
-                                pastOpp(wpb, nextRound))
-          
-          if(i == 3 && pp == T){
-            
-            #### AMTA Power - Protecting #####
-            
-            b <- amtatab$crit1[order(amtatab$crit1, decreasing = T)]
-            
-            #  Find Thresholds
-            first_out <- b[7] # Assuming ORCs
-
-            # Separate into Brackets
-            
-            sec <- which(amtatab$crit1 >= (first_out + 1.5))
-            sec <- c(sec, which(amtatab$crit1 <= (first_out - 1)))
-            
-            amta_secd_tab <- amtatab[sec, ]
-            amta_prim_tab <- amtatab[-sec, ]
-            
-            # Deal with Odd Numbes
-            if(nrow(amta_prim_tab) %% 2 == 1){
-              
-              amta_prim_tab <- amta_prim_tab[order(amta_prim_tab$crit1, decreasing = T),]
-              nr <- nrow(amta_prim_tab)
-              
-              if(abs(amta_prim_tab$crit1[nr] - b[6]) > 1){ # assuming ORCs
-                
-                amta_secd_tab <- amta_secd_tab[order(amta_secd_tab$crit1, decreasing = T),]
-                
-                amta_prim_tab <- rbind(amta_prim_tab,
-                                       amta_secd_tab[1,])
-                
-                amta_secd_tab <- amta_secd_tab[-1, ]
-                
-              } else {
-                
-                amta_secd_tab <- rbind(amta_secd_tab,
-                                       amta_prim_tab[nr, ])
-                
-                amta_prim_tab <- amta_prim_tab[-nr, ]
-                
-              }
-              
-            }
-            
-            # Inital Pairing
-            amta_prim_pair <- initialPair(amta_prim_tab, nextRound, coinR3, pp = T)
-            amta_secd_pair <- initialPair(amta_secd_tab, nextRound, coinR3, pp = F)
-            
-            # Resolve Impermissibles
-            amtaprimFinal <- impermissWrap(amta_prim_pair, amtaImpermiss, round = nextRound)
-            amtasecdFinal <- impermissWrap(amta_secd_pair, amtaImpermiss, round = nextRound)
-            
-            # Fix Trial Numbers
-            amtasecdFinal$trial <- amtasecdFinal$trial + nrow(amtaprimFinal)/2
-            
-            # Recombine
-            amtaFinal <- rbind(amtaprimFinal, amtasecdFinal)
-            
-            #### No Change to WPB ######
-            
-            # Initial Pairing
-            wpbPair  <- initialPair(wpbtab, nextRound, coinR3)
-            
-            # Resolve Impermissibles
-            wpbFinalPair  <- impermissWrap(wpbPair, wpbImpermiss, round = nextRound)
-            
-            
-          } else {
-            
-            # Initial Pairing
-            amtaPair <- initialPair(amtatab, nextRound, coinR3)
-            wpbPair  <- initialPair(wpbtab, nextRound, coinR3)
-            
-            # Resolve Impermissibles
-            amtaFinalPair <- impermissWrap(amtaPair, amtaImpermiss, round = nextRound)
-            wpbFinalPair  <- impermissWrap(wpbPair, wpbImpermiss, round = nextRound)
-            
-            
-          }
-          
-          # Add Final Pairs
-          x <- paste0("r", i + 1, c("side","opp"))
-          amta[, x] <- addPairs(amtaFinalPair)
-          wpb[, x] <- addPairs(wpbFinalPair)
-
-        }
-        
-      }
-      
-      amta$trial <- trial
-      wpb$trial <- trial
-      
-      amta$sdev <- sdev[k]
-      wpb$sdev <- sdev[k]
-      
-      amta.tot <- rbind(amta.tot, amta)
-      wpb.tot <- rbind(wpb.tot, wpb)
-      meta.tot <- rbind(meta.tot, meta)
-      
-      if(trial%%100 == 0) print(trial) 
-    }
-  }
-  
-  if(type != 'random'){
-    amta.tot$sdev <- sd(str)
-    wpb.tot$sdev <- sd(str)
-  }
-  
-  amta.tot$cat <- type
-  wpb.tot$cat <- type
-  
-  return(list(amta = amta.tot, wpb = wpb.tot))
-}
-
-
 #### Set-up ######
 # Generate Column Names
 genNames = function(type = "AMTA"){
@@ -265,7 +13,40 @@ genNames = function(type = "AMTA"){
   return(x)
 }
 
+createData <- function(typ, base = NULL){
+  
+  if(typ == 'AMTA'){
+    res <- data.frame(team = 1:num.teams)
+  } else if(typ == 'wpb'){
+    res <- data.frame(team = 1:num.teams, base = base)
+  }
+  
+  res[ ,genNames(typ)] <- NA
+  
+  return(res)
+}
+
 ##### Round 1: Random Pairing ######
+
+pairR1Wrap <- function(dat, typ){
+  r1 <- c("r1side", "r1opp")
+  
+  if (typ == 'power'){
+    dat[, r1]  <- r1power(amta)
+  } else if (typ == 'fold'){
+    dat[, r1] <- r1fold(amta)
+  } else if (typ == 'envelope'){
+    dat[, r1] <- r1envelope(amta)
+  } else {
+    dat[, r1] <- genR1(amta, num.teams)
+  }
+  
+  dat$r1side <- ifelse(dat$r1side == 1, 'P', 'D')
+  
+  return(dat)
+  
+}
+
 # For Round 1, assign teams randomly
 genR1 = function(mat1, num.teams){
   mat1$r1side <- sample(rep(0:1, num.teams/2), num.teams)
